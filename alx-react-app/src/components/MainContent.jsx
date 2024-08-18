dc07e4ce3d434491fc8de72d9991003896ffa085
yemmy28 <28episode@gmail.com>
2024-08-19 08:04:04 +0900

    Edited files in src components

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -6,8 +6,8 @@
 
 function MainContent() {
     return (
-        <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-            <h2 style={{ color: '#333' }}>{props.title}</h2>
+        <main>
+            <h2>{props.title}</h2>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
