f558ae53e9d5f6fe203fece7f495ea4cc7e38512
yemmy28 <28episode@gmail.com>
2024-08-19 08:26:41 +0900

    Enhancing components with CSS

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function UserProfile(props) {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
+      <p style={{ fontSize: '18px' }}>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p style={{ fontSize: '16px' }}>Bio: {props.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
+
+
