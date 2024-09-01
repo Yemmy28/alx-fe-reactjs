ad7a0077ac25093fdb099886c865134bc6caa24d
Ridwan <28episode@gmail.com>
2024-08-31 17:14:27 -0700

    Created react-router-advance

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
+
+
+function useAuth() {
+    // Simulating user authentication status
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+    // Logic to check if user is authenticated
+    const login = () => setIsAuthenticated(true);
+    const logout = () => setIsAuthenticated(false);
+  
+    return { isAuthenticated, login, logout };
+  }
+
 
 const ProtectedRoute = ({ isAuthenticated, children }) => {
   if (!isAuthenticated) {
