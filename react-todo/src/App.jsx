d60f3797d0c0c3dcb9d86874aabaa7c7c59955f6
Ridwan <28episode@gmail.com>
2024-09-07 23:53:17 -0700

    edited files in react-todo

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import TodoList from './components/TodoList';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +11,10 @@ function App() {
   return (
     <>
       <div>
+      <div className="App">
+      <h1>My Todo App</h1>
+      <TodoList /> 
+    </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
